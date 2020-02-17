42d5aec9673797b79802d7d402254d3281ed07ab
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-17 20:03:58 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,12 +17,12 @@ end
 def first_word_capitalized_and_ends_with_punctuation?(text)
   if text.match(/\A[A-Z][a-z A-Z\S]*\b(.)$/)
     return true
-    
   else
     false
   end
 end
 
 def valid_phone_number?(phone)
+  
 
 end
