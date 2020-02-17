91413c62044d807840c76f1afd5f274c484000ad
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-17 19:33:26 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -19,7 +19,8 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/\b[A-Z]\w*[,|.]/)
+  if text.match(/\A[A-Z]/)
+    if text.match(//)
 end
 
 def valid_phone_number?(phone)
