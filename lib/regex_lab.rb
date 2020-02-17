15d268a6824672d12a5893b4b108d6418847647c
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-17 19:26:08 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  if text.scan(/\A[A-Z][,|.|?|!|'|;]/)
+  if text.match(/\A[A-Z][,|.|?|!|'|;]/)
     return true
   else
     false
